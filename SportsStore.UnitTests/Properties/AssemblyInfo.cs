0838827be397d4d5f2397909e03cad72b5d605ba
Rohan Parkes <rparkes@email.com>
2020-12-15 20:09:05 +1100

    Add project files.

---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b210685-f848-4523-ac4d-454ceaa177f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.WebUI.Infrastructure.Abstract;
+using System.Web.Security;
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string userName, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(userName, password);
+            
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+            }
+
+            return result;
+        }
+    }
+}
